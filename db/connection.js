26e293651dafb4f8c7188d962a4b0336066fbb99
Jordan Williams <Oryinn@Jordans-MacBook-Pro-6.local>
2019-03-21 14:21:12 -0400

    Skeleton file structure created

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.once('open', () => {
+    console.log("Mongoose has connected to MONGODB")
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error: ', err)
+    process.exit(-1)
+})
+
+module.exports = mongoose
